8a5dcc87b3d8373f048ac969d75ff47c9937a316
Valentin <valentincaminos12345@gmail.com>
2024-04-23 10:07:08 -0300

    .

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -45,6 +45,7 @@ export function Filter() {
                         onChange={handleChangeCountry}
                     />
                 </div>
+                <div></div>
                 <input type="button" value='Search' onClick={handleClickLocation}/>
             </section>
 
